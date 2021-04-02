65d3ff43f724464ae79b6369f33fbb57e2badd06
Luis Augenstein <luis.augenstein@web.de>
2021-04-02 09:57:09 +0200

    created Home Link on detail calculator page

--- fronted/src/calculator/DetailHeader.js
+++ fronted/src/calculator/DetailHeader.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function DetailHeader(props) {
     return <section id="breadcrumbs" class="breadcrumbs">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
-                <h2>Portfoio Details</h2>
+                <h2>Caclulator Project</h2>
                 <ol>
-                    <li><a href="index.html">Home</a></li>
-                    <li>Portfoio Details</li>
+                    <li><Link to="/#hero">Home</Link></li>
+                    <li>Calculator</li>
                 </ol>
             </div>
 
