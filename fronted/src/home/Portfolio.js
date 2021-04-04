dbe69183cf3f551cbca78800f5955b0c18c17992
Luis Augenstein <luis.augenstein@web.de>
2021-04-04 14:32:28 +0200

    created SingleProject file

--- fronted/src/home/Portfolio.js
+++ fronted/src/home/Portfolio.js
@@ -1,8 +1,6 @@
-import { Box, Container, Divider, makeStyles, Typography, Grid, Card, CardActionArea, CardMedia } from '@material-ui/core'
+import { Box, Container, makeStyles, Typography, Grid } from '@material-ui/core'
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { MdAdd } from 'react-icons/md'
-import { fade } from '@material-ui/core/styles/colorManipulator';
+import SingleProject from './SingleProject';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -19,33 +17,6 @@ const useStyles = makeStyles(theme => ({
             background: theme.palette.primary.main
         }
     },
-    media: {
-        width: "100%",
-    },
-    card: {
-        position: "relative",
-        "&:hover #bluebox": {
-            bottom: "0"
-        }
-    },
-    actionarea: {
-        "&:hover $focusHighlight": {
-            opacity: "1"
-        }
-    },
-    focusHighlight: {
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-    },
-    bluebox: {
-        width: "100%",
-        height: "42px",
-        bottom: "-42px",
-        position: "absolute",
-        transition: "all ease-in-out 0.3s",
-        color: "#fff",
-        fontSize: "28px",
-        background: fade(theme.palette.primary.main, 0.75)
-    }
 }))
 
 function Portfolio(props) {
@@ -63,34 +34,14 @@ function Portfolio(props) {
         { src: "img/portfolio/portfolio-9.jpg", link: "/" },
     ]
 
-    const projects = images.map(img => {
-        return <Grid item md="4" sm="6" xs="12">
-            <Link to={img.link}>
-                <Card id="card" className={classes.card}>
-                    <CardActionArea classes={{
-                        root: classes.actionarea,
-                        focusHighlight: classes.focusHighlight
-                    }}>
-                        <img class={classes.media} src={img.src} alt="" />
-                    </CardActionArea>
-                    <Grid container id="bluebox" className={"portfolio-links " + classes.bluebox} justify="center" alignItems="center">
-                        <MdAdd />
-                    </Grid>
-                </Card>
-            </Link>
-        </Grid>
-    })
     return <Box id="portfolio">
         <Container className={classes.container} maxWidth="xl">
             <Typography variant="h3" className={classes.title}>
                 Portfolio
             </Typography>
-            <Divider />
-
             <Grid container spacing="4" data-aos="fade-up" data-aos-delay="100">
-                {projects}
+                {images.map(img => <SingleProject src={img.src} link={img.link} />)}
             </Grid>
-
         </Container>
     </Box>
 }
--- fronted/src/home/SingleProject.js
+++ fronted/src/home/SingleProject.js
@@ -1,17 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Grid, Card, CardActionArea, makeStyles } from '@material-ui/core'
+import { MdAdd } from 'react-icons/md'
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-function SingleProject(props) {
-    return <div class="col-lg-4 col-md-6 portfolio-item">
-        <Link to={props.link}>
-            <div class="portfolio-wrap">
-                <img src={props.src} class="img-fluid" alt="" />
-                <div class="portfolio-links">
-                    <a href="/" title="More Details"><i class="bx bx-plus"></i></a>
-                </div>
-            </div>
+const useStyles = makeStyles(theme => ({
+    media: {
+        width: "100%",
+    },
+    card: {
+        position: "relative",
+        "&:hover #bluebox": {
+            bottom: "0"
+        }
+    },
+    actionarea: {
+        "&:hover $focusHighlight": {
+            opacity: "1"
+        }
+    },
+    focusHighlight: {
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+    },
+    bluebox: {
+        width: "100%",
+        height: "42px",
+        bottom: "-42px",
+        position: "absolute",
+        transition: "all ease-in-out 0.3s",
+        color: "#fff",
+        fontSize: "28px",
+        background: fade(theme.palette.primary.main, 0.75)
+    }
+}))
+
+function SingleProject({ src, link }) {
+    const classes = useStyles()
+    return <Grid item md="4" sm="6" xs="12">
+        <Link to={link}>
+            <Card id="card" className={classes.card}>
+                <CardActionArea classes={{
+                    root: classes.actionarea,
+                    focusHighlight: classes.focusHighlight
+                }}>
+                    <img class={classes.media} src={src} alt="" />
+                </CardActionArea>
+                <Grid container id="bluebox" className={"portfolio-links " + classes.bluebox} justify="center" alignItems="center">
+                    <MdAdd />
+                </Grid>
+            </Card>
         </Link>
-    </div>
+    </Grid>
 }
 
 export default SingleProject
